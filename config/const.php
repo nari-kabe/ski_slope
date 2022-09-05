997508de650f58eb0cb92a5e8f683c48cc860832
nari-kabe <kanenari38@icloud.com>
2022-09-05 20:59:48 +0900

    API導入のみ。細かいところはまだ

--- config/const.php
+++ config/const.php
@@ -8,4 +8,9 @@ return [
         'access_token_secret' => env('TWITTER_CLIENT_ID_ACCESS_TOKEN_SECRET'),
         'call_back_url' => env('TWITTER_CALLBACK_URL'),
     ],
+    
+    'openweathermap' => [
+        'key' => env('OPEN_WHATHER_MAP_API_KEY'),
+        'url' => env('OPEN_WHATHER_MAP_URL'),
+    ]
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostController@home');
 Route::get('/pages/login', 'PostController@login');
@@ -22,3 +9,6 @@ Route::get('/pages/izumi', 'PostController@izumi');
 Route::get('/pages/star', 'PostController@star');
 Route::get('/pages/create_slope', 'PostController@create_slope');
 Route::get('/pages/login-home', 'PostController@login_home');
+
+//OpenWeatherAPI
+Route::get('/a', 'WeatherAPIController@weatherData');
