b0eb552a34d940d1d68673857fee7cf602cbb507
nari-kabe <kanenari38@icloud.com>
2022-09-08 09:44:23 +0900

    jsファイルの呼び出しとtwitterAPIのデータの取り出し方を変更

--- resources/views/pages/create_slope.blade.php
+++ resources/views/pages/create_slope.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>スキー場追加</title>
         <link rel="stylesheet" href="/css/create_slope.css">
-        
+        <script src="{{ asset('/js/create_slope.js') }}"></script>
     </head>
     <body>
         <form action="pages/login-home" method="POST">
@@ -104,17 +104,6 @@
                 </tr>
             </table>
         </div>
-        
-        <script>
-            function textarea_off(num){
-                /*document.getElementsByClassName("detail").style="display: none";*/
-                document.getElementById(num).style="display: none";
-            }
-                
-            function textarea_on(num){
-                document.getElementById(num).style="display: block";
-            }
-        </script>
         
         <hr>
         <h2>ゲレンデマップ</h2>
