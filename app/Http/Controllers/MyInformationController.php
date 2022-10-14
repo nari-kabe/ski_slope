c16270cef5b491277e4f7032976b9849ed8be805
nari-kabe <kanenari38@icloud.com>
2022-10-15 01:42:34 +0900

    スキー場とお気に入りの削除機能を追加

--- app/Http/Controllers/MyInformationController.php
+++ app/Http/Controllers/MyInformationController.php
@@ -14,7 +14,9 @@ class MyInformationController extends Controller
 {
     public function show(Profile $profile, Star $star)
     {
+        //プロフィールとお気に入りの表示
         $count = Star::count();
+        $all_star = Star::get(['id', 'place_id', 'user_id']);
         $place_id = null;
         $place_name = null;
         $my_profile = null;
@@ -28,23 +30,27 @@ class MyInformationController extends Controller
                 $my_star = Star::where('user_id', \Auth::user()->id)->first();
             }
         }
-            
+        
+        for($i = 0; $i < $count; $i++) {
+            if (Ski_area::find($all_star[$i]['place_id']) !== null) {
+                $part_star[] = $all_star[$i];
+            }
+        }
+        
         if ($my_profile !== null) {
-       
             if ($my_star !== null) {
-                for($i = 1; $i <= $count; $i++){
-                    if (Star::find($i) === null){
-                        $count += 1;
-                    } else if (Star::find($i) !== null && Star::find($i)['user_id'] === Auth::user()->id) {
-                        $stars[] = Star::find($i);
+                for($i = 0; $i < count($part_star); $i++) {
+                    if (Ski_area::find($part_star[$i]['user_id'] === Auth::user()->id)) {
+                        $stars[] = $part_star[$i];
                     }
                 }
                 for($i = 0; $i < count($stars); $i++){
-                        $place_id[] = $stars[$i]['place_id'];
-                        $place_name[] = Ski_area::where('id', '=', $place_id[$i])->first()['place_name'];
+                    $star_id[] = $stars[$i]['id'];
+                    $place_id[] = $stars[$i]['place_id'];
+                    $place_name[] = Ski_area::where('id', '=', $place_id[$i])->first()['place_name'];
                 }
-                
-                return view('pages/my_information')->with(['my_profile' => $my_profile, 'star'=>$star, 'place_id'=>$place_id, 'place_name'=>$place_name]);
+            
+                return view('pages/my_information')->with(['my_profile' => $my_profile, 'star'=>$star, 'star_id'=>$star_id, 'place_id'=>$place_id, 'place_name'=>$place_name]);
             }
             
             return view('pages/my_information')->with(['my_profile' => $my_profile, 'place_id'=>$place_id]);
@@ -70,6 +76,12 @@ class MyInformationController extends Controller
             return view('pages/my_information')->with(['place_id'=>$place_id]);
         }
         
+    }
+    
+    public function destroy($id){
+        $asd = Star::find($id);
+        $asd->delete();
+        return redirect('/pages/my_information');
     }
     
 }
--- public/js/login_home.js
+++ public/js/login_home.js
@@ -1,48 +1,48 @@
 const array = ["/images/sample1.jpeg", "/images/sample2.jpeg", "/images/sample3.jpeg", "/images/sample4.jpeg"];
-                const array2 = ["'radius1'", "'radius2'", "'radius3'", "'radius4'"];
-                let num = 0;
-                const elements = document.getElementsByName('radio_button');
-                
-                /*画像 横ボタン*/
-                function go_back(){
-                    elements[num].checked = false;
-                    
-                　　if (num == 0) {
-                        num = 3;
-                    }
-                    else {
-                        num --;
-                    }
-                    document.getElementById("image").src=array[num];
-                    elements[num].checked = true;
-            　　}
-                
-                function go_forward(){
-                    elements[num].checked = false;
-                    if (num == 3) {
-                        num = 0;
-                    }
-                    else {
-                        num ++;
-                    }
-                    document.getElementById("image").src=array[num];
-                    elements[num].checked = true;
-                }
-                
-                /*画像 下の丸ボタン*/
-                function radius1(){
-                    num=0;
-                    document.getElementById("image").src=array[0];
-                }
-                function radius2(){
-                    num=1;
-                    document.getElementById("image").src=array[1];
-                }
-                function radius3(){
-                    num=2;
-                    document.getElementById("image").src=array[2];
-                }
-                function radius4(){
-                    num=3;
-                    document.getElementById("image").src=array[3];
-                }
+const array2 = ["'radius1'", "'radius2'", "'radius3'", "'radius4'"];
+let num = 0;
+const elements = document.getElementsByName('radio_button');
+
+/*画像 横ボタン*/
+function go_back(){
+    elements[num].checked = false;
+    
+　　if (num == 0) {
+        num = 3;
+    }
+    else {
+        num --;
+    }
+    document.getElementById("image").src=array[num];
+    elements[num].checked = true;
+　　}
+
+function go_forward(){
+    elements[num].checked = false;
+    if (num == 3) {
+        num = 0;
+    }
+    else {
+        num ++;
+    }
+    document.getElementById("image").src=array[num];
+    elements[num].checked = true;
+}
+
+/*画像 下の丸ボタン*/
+function radius1(){
+    num=0;
+    document.getElementById("image").src=array[0];
+}
+function radius2(){
+    num=1;
+    document.getElementById("image").src=array[1];
+}
+function radius3(){
+    num=2;
+    document.getElementById("image").src=array[2];
+}
+function radius4(){
+    num=3;
+    document.getElementById("image").src=array[3];
+}
